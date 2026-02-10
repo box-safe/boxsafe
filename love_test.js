dbd45b302293a633b0f3d8cdcdee63be334736d0
DiegoZaluski <diegozaluski@icloud.com>
2026-02-09 23:16:04 -0300

    save agent

--- love_test.js
+++
@@ -1,6 +0,0 @@
-import { spawnSync } from 'node:child_process';
-import path from 'node:path';
-const target = path.join(path.dirname(new URL(import.meta.url).pathname), 'love_test.cjs');
-const res = spawnSync('node', [target], { stdio: 'inherit' });
-if (res.error) throw res.error;
-process.exit(res.status ?? 0);
