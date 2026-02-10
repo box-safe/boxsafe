62036ada45ba05dabc9c5fdc8736e794d956317c
DiegoZaluski <diegozaluski@icloud.com>
2026-02-09 23:18:32 -0300

    save agent

--- love_test.js
+++
@@ -1,9 +0,0 @@
-const platform = process.platform;
-
-if (platform === 'linux') {
-  console.log('I Love you');
-} else if (platform === 'win32') {
-  console.log('I Hate you');
-} else {
-  console.log(`Operating system detected: ${platform}. This script only knows how to love Linux and hate Windows.`);
-}
