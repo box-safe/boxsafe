07bb9aff98992f0ddc54d74cd33a10d4c6a9916b
DiegoZaluski <diegozaluski@icloud.com>
2026-02-09 22:44:09 -0300

    save agent

--- test-output.js
+++
@@ -1,7 +0,0 @@
-if (process.platform === 'linux') {
-  console.log('I Love you');
-} else if (process.platform === 'win32') {
-  console.log('I Hate you');
-} else {
-  console.log('Unknown OS, but my feelings are neutral.');
-}
